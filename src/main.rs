eaa52f56df2bfd166285ece10f140c7ecc287d86
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-02 21:56:35 -0500

    refactor: Break generate_callgraph into smaller functions
    This function was getting big and hard to understand. Splitting
    thefunctionality into `generate_clusters` and `generate_links` seems to
    be a lot cleaner.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ struct Function {
 }
 
 impl Function {
+    const fn new() -> Self {
+        Self {
+            name: String::new(),
+            body: String::new(),
+            module: String::new(),
+        }
+    }
+
     fn body_length(&self) -> usize {
         self.body.lines().count()
     }
@@ -20,97 +28,90 @@ impl Function {
             .is_match(&self.body)
     }
 
-    fn clear(&mut self) {
-        self.name.clear();
-        self.body.clear();
-        self.module.clear();
-    }
-
     fn link(&self, f: &Self) -> String {
         format!(r#""{}" -> "{}";"#, self.label(), f.label())
     }
 }
 
 fn extract_functions(filenames: &[String], start: &str, end: &str) -> Vec<Function> {
-    let mut functions = Vec::new();
+    filenames
+        .iter()
+        .fold(Vec::new(), |mut functions, filename| {
+            let source_code = std::fs::read_to_string(filename)
+                .unwrap_or_else(|_| panic!("Failed to read file: {filename}"));
 
-    for filename in filenames {
-        let source_code = std::fs::read_to_string(filename)
-            .unwrap_or_else(|_| panic!("Failed to read file: {filename}"));
+            let mut function = Function::new();
+            for line in source_code.lines() {
+                if line.starts_with(start) {
+                    function = Function {
+                        name: line[3..line
+                            .find('(')
+                            .expect("No opening parenthesis found in the function signature")]
+                            .to_string(),
+                        body: String::from("{"),
+                        module: String::from(filename),
+                    };
+                    continue;
+                }
 
-        let mut function = Function {
-            name: String::new(),
-            body: String::new(),
-            module: String::new(),
-        };
+                if !function.name.is_empty() {
+                    function.body.push_str(line);
+                    function.body.push('\n');
 
-        for line in source_code.lines() {
-            if line.starts_with(start) {
-                function = Function {
-                    name: line[3..line
-                        .find('(')
-                        .expect("No opening parenthesis found in the function signature")]
-                        .to_string(),
-                    body: String::from("{"),
-                    module: String::from(filename),
-                };
-                continue;
-            }
-
-            if !function.name.is_empty() {
-                function.body.push_str(line);
-                function.body.push('\n');
-
-                if line.starts_with(end) {
-                    functions.push(function.clone());
-                    function.clear();
+                    if line.starts_with(end) {
+                        functions.push(function);
+                        function = Function::new();
+                    }
                 }
             }
-        }
-    }
 
+            functions
+        })
+}
+
+fn generate_cluster(module: &str, functions: &[Function]) -> String {
+    "subgraph cluster_".to_string()
+        + &module.replace(['.', '-', '/'], "_")
+        + "{label = \""
+        + module
+        + "\";"
+        + &functions
+            .iter()
+            .filter(|f| f.module == *module)
+            .fold(String::new(), |acc, f| acc + "\"" + &f.label() + "\";")
+        + "}"
+}
+
+fn generate_clusters(functions: &[Function]) -> String {
     functions
-}
-
-fn sanitize_filename(filename: &str) -> String {
-    filename.replace(['.', '-', '/'], "_")
-}
-
-fn generate_callgraph(filenames: &[String], start: &str, end: &str) -> String {
-    let functions = extract_functions(filenames, start, end);
-    let modules = functions
         .iter()
         .map(|f| f.module.clone())
         .collect::<std::collections::HashSet<_>>()
         .into_iter()
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|module| generate_cluster(module, functions))
+        .collect::<String>()
+}
 
+fn generate_links(functions: &[Function]) -> String {
+    functions
+        .iter()
+        .map(|f1| {
+            functions
+                .iter()
+                .filter(|f2| f1.check_for_function_in_body(f2))
+                .map(|f2| f1.link(f2))
+                .collect::<String>()
+        })
+        .collect::<String>()
+}
+
+fn generate_callgraph(filenames: &[String], start: &str, end: &str) -> String {
+    let functions = extract_functions(filenames, start, end);
     String::from("strict digraph {graph [rankdir=LR];node [shape=box];")
-        + &modules
-            .iter()
-            .map(|module| {
-                "subgraph cluster_".to_string()
-                    + &sanitize_filename(module)
-                    + "{label = \""
-                    + module
-                    + "\";"
-                    + &functions
-                        .iter()
-                        .filter(|f| f.module == *module)
-                        .fold(String::new(), |acc, f| acc + "\"" + &f.label() + "\";")
-                    + "}"
-            })
-            .collect::<String>()
-        + &functions
-            .iter()
-            .map(|f1| {
-                functions
-                    .iter()
-                    .filter(|f2| f1.check_for_function_in_body(f2))
-                    .map(|f2| f1.link(f2))
-                    .collect::<String>()
-            })
-            .collect::<String>()
+        + generate_clusters(&functions).as_str()
+        + generate_links(&functions).as_str()
         + "}"
 }
 
