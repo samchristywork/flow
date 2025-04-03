4c3f907190d6c9249696018d3981eb1d81c39afa
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-02 20:54:30 -0500

    feat: Add extract_functions function
    This function creates a Vector of functions from a `&str` of source
    code. It does this by searching for a function start pattern and end
    pattern.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Clone)]
 struct Function {
     name: String,
     body: String,
@@ -17,4 +18,45 @@ impl Function {
             .expect("Invalid regex pattern")
             .is_match(&self.body)
     }
+
+    fn clear(&mut self) {
+        self.name.clear();
+        self.body.clear();
+    }
+}
+
+fn extract_functions(source_code: &str, start_pattern: &str, end_pattern: &str) -> Vec<Function> {
+    let mut function = Function {
+        name: String::new(),
+        body: String::new(),
+    };
+
+    let mut functions = Vec::new();
+    for line in source_code.lines() {
+        if line.starts_with(start_pattern) {
+            function = Function {
+                name: line[3..line
+                    .find('(')
+                    .expect("No opening parenthesis found in the function signature")]
+                    .to_string(),
+                body: String::from("{"),
+            };
+            continue;
+        }
+
+        if !function.name.is_empty() {
+            function.body.push_str(line);
+            function.body.push('\n');
+
+            if line.starts_with(end_pattern) {
+                functions.push(function.clone());
+                function.clear();
+            }
+        }
+    }
+
+    functions
+}
+
+fn main() {
 }
